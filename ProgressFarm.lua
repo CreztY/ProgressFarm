6df8936b6beb79067e86c7b3edc351c83d9a821d
CreztY <ivi.cozar01@gmail.com>
2024-06-27 09:42:49 +0200

    1.2

--- ProgressFarm.lua
+++ ProgressFarm.lua
@@ -2,10 +2,15 @@ local items = {
   "Mena de cobalto",
   "Mena de saronita",
   "Mena de obsidium",
-  "Mena de elementium"
+  "Mena de elementium",
+  "Tierra volátil",
+  "Fuego volátil",
+  "Agua volátil"
 }
 
+local trackedItems = {}
 local selectedItem = nil
+local progressBarCount = 0
 
 local frame = CreateFrame("Frame", "ProgressFarmFrame", UIParent, "BasicFrameTemplateWithInset")
 frame:SetSize(300, 250)
@@ -25,7 +30,7 @@ frame.title:SetPoint("CENTER", frame.TitleBg, "CENTER", 5, 0)
 frame.title:SetText("ProgressFarm")
 
 local selectedItemText = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-selectedItemText:SetPoint("TOP", frame, "TOP", 0, -40)  -- Ajusta el punto de anclaje aquí
+selectedItemText:SetPoint("TOP", frame, "TOP", 0, -40)
 selectedItemText:SetText("Selecciona un objeto")
 
 local dropdown = CreateFrame("Frame", nil, frame, "UIDropDownMenuTemplate")
@@ -40,30 +45,80 @@ maxInputBox:SetAutoFocus(false)
 maxInputBox:SetText("0")
 maxInputBox:Hide()
 
-local progressBar = CreateFrame("StatusBar", nil, frame, "TextStatusBar")
-progressBar:SetSize(200, 20)
-progressBar:SetPoint("TOP", maxInputBox, "BOTTOM", 0, -20)
-progressBar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-progressBar:SetStatusBarColor(0, 1, 0) -- Verde
-progressBar:SetMinMaxValues(0, 1)
-progressBar:SetValue(0)
-progressBar:Hide()
-
-local progressBarBg = progressBar:CreateTexture(nil, "BACKGROUND")
-progressBarBg:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Background")
-progressBarBg:SetAllPoints(true)
-
-local progressText = progressBar:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-progressText:SetFont("Fonts\\FRIZQT__.TTF", 10)
-progressText:SetPoint("CENTER", progressBar)
-
 local farmButton = CreateFrame("Button", nil, frame, "GameMenuButtonTemplate")
-farmButton:SetPoint("TOP", progressBar, "BOTTOM", 0, -10)
+farmButton:SetPoint("TOP", maxInputBox, "BOTTOM", 0, -10)
 farmButton:SetSize(140, 40)
 farmButton:SetText("Farmear")
 farmButton:SetNormalFontObject("GameFontNormalLarge")
 farmButton:SetHighlightFontObject("GameFontHighlightLarge")
 farmButton:Disable()
+
+local progressBarContainer = _G["progressBarContainer"]
+progressBarContainer:SetSize(300, 400)
+progressBarContainer:SetPoint("CENTER")
+progressBarContainer:Hide()
+
+-- Añadir la capacidad de mover el contenedor
+progressBarContainer:SetMovable(true)
+progressBarContainer:EnableMouse(true)
+progressBarContainer:RegisterForDrag("LeftButton")
+progressBarContainer:SetScript("OnDragStart", progressBarContainer.StartMoving)
+progressBarContainer:SetScript("OnDragStop", progressBarContainer.StopMovingOrSizing)
+
+local function CreateProgressBar(item, maxCount, index)
+  local progressBarPanel = CreateFrame("Frame", nil, progressBarContainer)
+  progressBarPanel:SetSize(280, 60)
+  progressBarPanel:SetPoint("TOP", 0, -(index * 70) - 10)
+
+  local progressBarName = progressBarPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  progressBarName:SetPoint("TOP", progressBarPanel, "TOP", 0, -10)
+  progressBarName:SetText(item)
+
+  local progressBar = CreateFrame("StatusBar", nil, progressBarPanel, "TextStatusBar")
+  progressBar:SetSize(200, 20)
+  progressBar:SetPoint("TOP", progressBarName, "BOTTOM", 0, -10)
+  progressBar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+  progressBar:SetStatusBarColor(0, 1, 0) -- Verde
+  progressBar:SetMinMaxValues(0, maxCount)
+  progressBar:SetValue(0)
+
+  local progressBarBg = progressBar:CreateTexture(nil, "BACKGROUND")
+  progressBarBg:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Background")
+  progressBarBg:SetAllPoints(true)
+
+  local progressText = progressBar:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  progressText:SetFont("Fonts\\FRIZQT__.TTF", 10)
+  progressText:SetPoint("CENTER", progressBar)
+
+  local deleteButton = CreateFrame("Button", nil, progressBarPanel, "UIPanelButtonTemplate")
+  deleteButton:SetSize(20, 20)
+  deleteButton:SetText("-")
+  deleteButton:SetPoint("LEFT", progressBar, "RIGHT", 10, 0)
+
+  deleteButton:SetScript("OnClick", function()
+    progressBarPanel:Hide()
+    trackedItems[item] = nil
+    -- Reajustar las posiciones de las barras de progreso
+    local newIndex = 0
+    for _, panel in pairs(trackedItems) do
+      panel:SetPoint("TOP", 0, -(newIndex * 70) - 10)
+      newIndex = newIndex + 1
+    end
+    progressBarCount = newIndex -- Actualizar el contador
+  end)
+
+  progressBarPanel:SetScript("OnUpdate", function(self)
+    local currentCount = GetItemCount(item)
+    progressBar:SetValue(currentCount)
+    progressText:SetText(currentCount .. " / " .. maxCount)
+    if currentCount >= maxCount then
+      self:SetScript("OnUpdate", nil)
+      print("¡Has alcanzado el máximo de " .. item .. "!")
+    end
+  end)
+
+  return progressBarPanel
+end
 
 local function ValidateForm()
   if selectedItem and tonumber(maxInputBox:GetText()) and tonumber(maxInputBox:GetText()) > 0 then
@@ -104,24 +159,26 @@ function frame:Reset()
   maxInputBox:SetText("0")
   maxInputBox:Hide()
   farmButton:Disable()
-  progressBar:Hide()
-  progressBar:SetValue(0)
+end
+
+function frame:FarmAccept()
+  selectedItem = nil
+  selectedItemText:SetText("")
+  maxInputBox:SetText("0")
+  maxInputBox:Hide()
+  farmButton:Disable()
 end
 
 farmButton:SetScript("OnClick", function()
+  PlaySound(856)
   local maxCount = tonumber(maxInputBox:GetText())
-  progressBar:SetMinMaxValues(0, maxCount)
-  progressBar:Show()
-  local itemCount = GetItemCount(selectedItem)
-  progressBar:SetValue(itemCount)
-  progressText:SetText(itemCount .. " / " .. maxCount)
-  frame:SetScript("OnUpdate", function(self)
-    local currentCount = GetItemCount(selectedItem)
-    progressBar:SetValue(currentCount)
-    progressText:SetText(currentCount .. " / " .. maxCount)
-    if currentCount >= maxCount then
-      self:SetScript("OnUpdate", nil)
-      print("¡Has alcanzado el máximo de " .. selectedItem .. "!")
-    end
-  end)
+  if not trackedItems[selectedItem] then
+    print(progressBarCount) -- Esto imprimirá el número correcto de barras de progreso
+    local progressBarPanel = CreateProgressBar(selectedItem, maxCount, progressBarCount)
+    trackedItems[selectedItem] = progressBarPanel
+    progressBarPanel:Show()
+    progressBarCount = progressBarCount + 1 -- Incrementar el contador
+  end
+  progressBarContainer:Show()
+  frame:FarmAccept()
 end)
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # ProgressFarm
- wow addon ProgressFarm
+
+## Description:
+
+ProgressFarm is a tool for World of Warcraft players who want to track and manage their farming progress for various items efficiently and visually appealing. This addon is designed to provide an intuitive user interface that allows players to monitor multiple farming goals simultaneously.
+
+## Main Features:
+
+### Dynamic Progress Bar:
+- Automatic creation of progress bars for each selected item, showing current progress and farming goals.
+- The progress bars are easily recognizable and visually appealing, with colors and text that clearly indicate the current status.
+
+### Real-Time Updates:
+- The progress bars update in real-time, reflecting any changes in the quantity of the farmed item.
+- Visual and audio notifications when the farming goal is reached.
+
+### Custom Organization:
+- Progress bars can be easily reorganized by dragging and dropping.
+- Delete buttons to remove any progress bars that are no longer needed.
+
+## Instructions for Use:
+
+### Installation:
+    1. Download the addon from CurseForge.
+    2. Extract the downloaded files into your World of Warcraft `Interface/AddOns` directory.
+    3. Restart World of Warcraft if it is currently running.
+
+### Open the Addon:
+Use the `/farm` command in the game chat to open the main ProgressFarm frame.
+
+### Select an Item:
+- Use the dropdown menu to select the item you want to track.
+- Enter the target quantity in the provided text box.
+
+### Start Tracking:
+- Click the "Farmear" button to start tracking the farming progress of the selected item.
+- A progress bar will be automatically created in the bar container.
+
+## Additional Notes:
+ProgressFarm is continuously developing and improving. We appreciate any comments or suggestions for future versions. Please feel free to contact us through our support page on CurseForge to report any issues or request new features.
+
+Thank you for using ProgressFarm and happy farming!
+
+## Author
+
+- [@CreztY](https://www.github.com/CreztY)
+
